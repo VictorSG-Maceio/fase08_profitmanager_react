8e92e2635a723349ee557f28532e3cbb57b9f77e
victor maceio <victor.maceio@supergeeks.com.br>
2023-03-24 15:49:23 -0300

    Refatorado

--- src/User.js
+++ src/User.js
@@ -1,32 +1,20 @@
 import './style.css';
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import PubSub from 'pubsub-js';
 import $ from 'jquery';
 import CustomInput from './components/CustomInput';
 import ManageErrors from './ManageErrors';
 
-export class UserForm extends Component {
+export function UserForm() {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      lista : [ ],
-      name : '',
-      email : '',
-      password : '',
-      password_confirmation : '',
-      guardaDados : {}
-    }
-    this.enviaForm = this.enviaForm.bind(this);
-    this.setName = this.setName.bind(this);
-    this.setEmail = this.setEmail.bind(this);
-    this.setPassword = this.setPassword.bind(this);
-    this.setPasswordConfirmation = this.setPasswordConfirmation.bind(this);
+  const [lista, setLista] = useState([]);
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [password_confirmation, setPassword_confirmation] = useState('');
+  const [guardaDados, setGuardaDados] = useState({});
 
-    this.guardaDados = {};
-  }
-
-  enviaForm(evento) {
+  function enviaForm(evento) {
     evento.preventDefault();
     console.log("dados sendo enviados...");
 
@@ -40,42 +28,47 @@ export class UserForm extends Component {
       type: 'post',
       data: JSON.stringify(
         {
-          name: this.state.name,
-          email: this.state.email,
-          password: this.state.password,
-          password_confirmation: this.state.password_confirmation
+          name: nome,
+          email: email,
+          password: password,
+          password_confirmation: password_confirmation
         }
       ),
 
       success: function(resposta) {
-        this.guardaDados = {};
         console.log("Sucesso!");
         console.log(resposta);
 
+        var obj = guardaDados;
         $.each(resposta.data, function(index, value) {
-          this.guardaDados[index] = value;
-        }.bind(this));
+          obj[index] = value;
+        });
+        setGuardaDados(obj);
 
         setTimeout(function(){
-          var  novaLista = this.state.lista;
-          novaLista.push(this.guardaDados);
-          //this.setState({lista: novaLista});
+          var novaLista = lista;
+          novaLista.push(guardaDados);
 
           PubSub.publish('atualiza-lista-usuarios', novaLista);
           alert("Cadastro realizado com sucesso!");
-          this.setState({name:'', email: '', password:'', password_confirmation:''});
-
-          this.guardaDados = {};
-        }.bind(this), 10);
+          setNome('');
+          setEmail('');
+          setPassword('');
+          setPassword_confirmation('');
+          setGuardaDados({});
+          setLista(novaLista);
+        }, 10);
       },
 
       complete: function(resposta) {
         console.log("Complete!!");
         console.log(resposta.getAllResponseHeaders());
-        this.guardaDados.token = resposta.getResponseHeader('access-token');
-        this.guardaDados.client = resposta.getResponseHeader('client');
-        this.guardaDados.uid = resposta.getResponseHeader('uid');
-      }.bind(this),
+        var obj = guardaDados;
+        obj.token = resposta.getResponseHeader('access-token');
+        obj.client = resposta.getResponseHeader('client');
+        obj.uid = resposta.getResponseHeader('uid');
+        setGuardaDados(obj);
+      },
       
       error: function(resposta) {
         if (resposta.status === 422) {
@@ -84,87 +77,70 @@ export class UserForm extends Component {
       }
     });
   }
+  
+  return (
+    <div>						
+      <h1 class="h2">Cadastro de Usuários</h1>						
+      <form method="post" onSubmit={enviaForm}>
+        <CustomInput type="text" id="name" label="Nome" 
+            name="name" placeholder="Nome"
+            value={nome} onChange={e => setNome(e.target.value)}/>
 
-  setName(evento){
-    this.setState( { name : evento.target.value } );
-  }
-  setEmail(evento){
-    this.setState( { email : evento.target.value } );
-  }
-  setPassword(evento){
-    this.setState( { password : evento.target.value } );
-  }
-  setPasswordConfirmation(evento){
-    this.setState( { password_confirmation : evento.target.value } );
-  }
-
-  render () {
-    return (
-      <div>						
-        <h1 class="h2">Cadastro de Usuários</h1>						
-        <form method="post" onSubmit={this.enviaForm}>
-          <CustomInput type="text" id="name" label="Nome" 
-              name="name" placeholder="Nome"
-              value={this.state.name} onChange={this.setName}/>
-          <CustomInput type="email" id="email" label="E-mail" 
-              name="email" placeholder="E-mail" 
-              value={this.state.email} onChange={this.setEmail}/>
-          <CustomInput  type="password" id="password" label="Senha" 
-              name="password" placeholder="Senha"
-              value={this.state.password} onChange={this.setPassword}/>
-          <CustomInput type="password" id="password_confirmation" label="Confirmação" 
-              name="password_confirmation" placeholder="Confirme"
-              value={this.state.password_confirmation} 
-              onChange={this.setPasswordConfirmation} />
-          <button type="submit" class="btn btn-primary">Inscrever-se</button>
-        </form>						
-      </div>
-    );
-  }
+        <CustomInput type="email" id="email" label="E-mail" 
+            name="email" placeholder="E-mail" 
+            value={email} onChange={e => setEmail(e.target.value)}/>
+        <CustomInput  type="password" id="password" label="Senha" 
+            name="password" placeholder="Senha"
+            value={password} onChange={e => setPassword(e.target.value)}/>
+        <CustomInput type="password" id="password_confirmation" label="Confirmação" 
+            name="password_confirmation" placeholder="Confirme"
+            value={password_confirmation} 
+            onChange={e => setPassword_confirmation(e.target.value)} />
+        <button type="submit" class="btn btn-primary">Inscrever-se</button>
+      </form>						
+    </div>
+  );
 }
-export class UserTable  extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {lista : []}
-  }
 
-  componentDidMount(){
-    PubSub.subscribe('atualiza-lista-usuarios', function(topico, novaLista){
+export function UserTable() {
+  const [lista, setLista] = useState([]);
+
+  useEffect(() => {
+    PubSub.subscribe('atualiza-lista-usuarios', (topico, novaLista) => {
       console.log("novaLista!!!!!!");
-      this.setState({lista : novaLista});
-    }.bind(this));
+      setLista(novaLista);
+    });
 
-    PubSub.subscribe('erro-validacao', function(topico, erro){
+    PubSub.subscribe('erro-validacao', (topico, erro) => {
       alert(erro);
     });
-  }
-  render(){
-    return(
-      <div class="table-responsive">
-        <h2>Usuários</h2>
-        <table class="table table-striped table-sm">
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>Name</th>
-              <th>E-mail</th>
-            </tr>
-          </thead>
-          <tbody>
-            {
-              this.state.lista.map(function(user){
-                return(
-                  <tr>
-                    <td>{user.id}</td>
-                    <td>{user.nome}</td>
-                    <td>{user.email}</td>
-                  </tr>
-                );
-              })
-            }							
-          </tbody>
-        </table>
-      </div>
-    );
-  }
+  });
+
+  return(
+    <div class="table-responsive">
+      <h2>Usuários</h2>
+      <table class="table table-striped table-sm">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>E-mail</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            lista.map((user) => {
+              return(
+                <tr>
+                  <td>{user.id}</td>
+                  <td>{user.nome}</td>
+                  <td>{user.email}</td>
+                </tr>
+              );
+            })
+          }							
+        </tbody>
+      </table>
+    </div>
+  );
 }
